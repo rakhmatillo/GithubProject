80d17e21a0b106d140abcea4109f5533a9bb347f
coder7504 <asliddin2703@gmail.com>
2021-06-26 18:50:32 +0500

    added  Asliddin view controller

---
+++ GithubProject/AsliddinViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AsliddinViewController.swift
+//  GithubProject
+//
+//  Created by Asliddin Mahmudov on 6/26/21.
+//
+
+import UIKit
+
+class AsliddinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+ 
+}
