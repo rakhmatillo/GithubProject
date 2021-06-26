d140bdfcf96ec11e40b9e4e03cdd0bf3add28d84
Asqar Ibodov <asqardif@gmail.com>
2021-06-26 18:49:30 +0500

    firdavsVC

---
+++ GithubProject/FirdavsVC.swift
@@ -0,0 +1,20 @@
+//
+//  FirdavsVC.swift
+//  GithubProject
+//
+//  Created by Zokirov Firdavs on 26/06/21.
+//
+
+import UIKit
+
+class FirdavsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+  
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+}
