6ed2d391873ca43fa4415e938c4afe6326cd0a52
Nurillo <nurilloumarov979@gmail.com>
2021-06-26 18:57:04 +0500

    salom

---
+++ NurilloVC.swift
@@ -0,0 +1,29 @@
+//
+//  NurilloVC.swift
+//  GithubProject
+//
+//  Created by apple on 26/06/21.
+//
+
+import UIKit
+
+class NurilloVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
